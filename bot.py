607e7930c2a652e3d2264d76215d8d2d20177786
Semmargl <decebel1995@gmail.com>
2023-12-03 20:50:00 +0000

    cav

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
-    if message.text == "Cкажы секрет":
-        bot.send_message(message.chat.id, "Серега душка")
+    if message.text == 'Cкажы секрет':
+        bot.send_message(message.chat.id, 'Серега душка')
     else:
         bot.send_message(message.chat.id, message.text)
 
